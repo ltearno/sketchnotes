31201a7202059f4ee382938389b69d14d5d7fe06
Arnaud Tournier <ltearno@gmail.com>
2018-05-27 17:02:52 +0200

    sdl2 project init

---
+++ test101/main.c
@@ -0,0 +1,217 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <math.h>
+
+#define GLEW_STATIC
+#include <GL/glew.h>
+#include <GL/glu.h>
+#include <GL/gl.h>
+
+#include <SDL2/SDL.h>
+
+void display() {
+   glClear(GL_COLOR_BUFFER_BIT);   // Clear the color buffer with current clearing color
+ 
+   // Define shapes enclosed within a pair of glBegin and glEnd
+   glBegin(GL_QUADS);              // Each set of 4 vertices form a quad
+      glColor3f(1.0f, 0.0f, 0.0f); // Red
+      glVertex2f(-0.8f, 0.1f);     // Define vertices in counter-clockwise (CCW) order
+      glVertex2f(-0.2f, 0.1f);     //  so that the normal (front-face) is facing you
+      glVertex2f(-0.2f, 0.7f);
+      glVertex2f(-0.8f, 0.7f);
+ 
+      glColor3f(0.0f, 1.0f, 0.0f); // Green
+      glVertex2f(-0.7f, -0.6f);
+      glVertex2f(-0.1f, -0.6f);
+      glVertex2f(-0.1f,  0.0f);
+      glVertex2f(-0.7f,  0.0f);
+ 
+      glColor3f(0.2f, 0.2f, 0.2f); // Dark Gray
+      glVertex2f(-0.9f, -0.7f);
+      glColor3f(1.0f, 1.0f, 1.0f); // White
+      glVertex2f(-0.5f, -0.7f);
+      glColor3f(0.2f, 0.2f, 0.2f); // Dark Gray
+      glVertex2f(-0.5f, -0.3f);
+      glColor3f(1.0f, 1.0f, 1.0f); // White
+      glVertex2f(-0.9f, -0.3f);
+   glEnd();
+ 
+   glBegin(GL_TRIANGLES);          // Each set of 3 vertices form a triangle
+      glColor3f(0.0f, 0.0f, 1.0f); // Blue
+      glVertex2f(0.1f, -0.6f);
+      glVertex2f(0.7f, -0.6f);
+      glVertex2f(0.4f, -0.1f);
+ 
+      glColor3f(1.0f, 0.0f, 0.0f); // Red
+      glVertex2f(0.3f, -0.4f);
+      glColor3f(0.0f, 1.0f, 0.0f); // Green
+      glVertex2f(0.9f, -0.4f);
+      glColor3f(0.0f, 0.0f, 1.0f); // Blue
+      glVertex2f(0.6f, -0.9f);
+   glEnd();
+ 
+   glBegin(GL_POLYGON);            // These vertices form a closed polygon
+      glColor3f(1.0f, 1.0f, 0.0f); // Yellow
+      glVertex2f(0.4f, 0.2f);
+      glVertex2f(0.6f, 0.2f);
+      glVertex2f(0.7f, 0.4f);
+      glVertex2f(0.6f, 0.6f);
+      glVertex2f(0.4f, 0.6f);
+      glVertex2f(0.3f, 0.4f);
+   glEnd();
+ 
+   //glFlush();  // Render now
+}
+
+int main(int argc, char *argv[])
+{
+    SDL_bool quit;
+
+    SDL_Window* window;
+    SDL_GLContext glContext;
+    SDL_Event sdlEvent;
+
+    quit = SDL_FALSE;
+
+    //Use OpenGL 3.1 core
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+
+    // Initialize video subsystem
+    if(SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
+        // Display error message
+        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+        return 0;
+    }
+    else
+    {
+        // Create window
+        window = SDL_CreateWindow("Hello World!", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE );
+        if( window == NULL )
+        {
+            // Display error message
+            printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
+            return 0;
+        }
+        else
+        {
+            // Create OpenGL context
+            glContext = SDL_GL_CreateContext(window);
+
+            if( glContext == NULL )
+            {
+                // Display error message
+                printf( "OpenGL context could not be created! SDL Error: %s\n", SDL_GetError() );
+                return 0;
+            }
+            else
+            {
+                // Initialize glew
+                glewInit();
+            }
+        }
+    }
+
+    // Game loop
+    while (!quit)
+    {
+        while(SDL_PollEvent(&sdlEvent) != 0)
+        {
+            // Esc button is pressed
+            switch(sdlEvent.type)
+            {
+                case SDL_QUIT:
+                    quit = SDL_TRUE;
+                    break;
+                
+                case SDL_FINGERMOTION:
+                    printf("Current finger position is: (%f, %f, %f)\n", sdlEvent.tfinger.x, sdlEvent.tfinger.y, sdlEvent.tfinger.pressure);
+                    break;
+                
+                case SDL_MOUSEMOTION:
+                    printf("Current mouse position is: (%d, %d)\n", sdlEvent.motion.x, sdlEvent.motion.y);
+                    break;
+            }
+        }
+
+        // Set background color as cornflower blue
+        glClearColor(0.39f, 0.58f, 0.93f, 1.f);
+        // Clear color buffer
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        display();
+
+        // Update window with OpenGL rendering
+        SDL_GL_SwapWindow(window);
+    }
+
+    //Destroy window
+    SDL_DestroyWindow(window);
+    window = NULL;
+
+    //Quit SDL subsystems
+    SDL_Quit();
+
+    return 0;
+}
+
+int oldmain(int argc, char *argv[])
+{
+    if (SDL_Init(SDL_INIT_VIDEO) != 0)
+    {
+        printf("SDL_Init Error: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    SDL_Window *win = SDL_CreateWindow("Hello World!", 100, 100, 640, 480, SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
+    if (win == NULL)
+    {
+        printf("SDL_CreateWindow Error: %s\n", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Renderer *ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    if (ren == NULL)
+    {
+        SDL_DestroyWindow(win);
+        printf("SDL_CreateRenderer Error: %s\n", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Event e;
+    SDL_bool quit = SDL_FALSE;
+    while (!quit)
+    {
+        while (SDL_PollEvent(&e))
+        {
+            if (e.type == SDL_QUIT)
+            {
+                quit = SDL_TRUE;
+            }
+            if (e.type == SDL_KEYDOWN)
+            {
+                quit = SDL_TRUE;
+            }
+            if (e.type == SDL_MOUSEBUTTONDOWN)
+            {
+                quit = SDL_TRUE;
+            }
+        }
+        //Render the scene
+        SDL_RenderClear(ren);
+        //renderTexture(image, renderer, x, y);
+        SDL_RenderPresent(ren);
+    }
+
+    //SDL_DestroyTexture(tex);
+    SDL_DestroyRenderer(ren);
+    SDL_DestroyWindow(win);
+    SDL_Quit();
+
+    return 0;
+}
